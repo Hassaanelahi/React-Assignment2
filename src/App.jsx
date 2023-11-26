6953c9a49006c6e1a40a404288cea0daed715403
Hassaan Elahi <107235334+Hassaanelahi@users.noreply.github.com>
2023-11-26 09:11:44 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './assets/Components/Navbar'
+import Main from './assets/Components/Main'
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import SecSection from './SecSection'
+import ThrSection from './ThrSection'
+
+
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+            <SecSection/>
+            <ThrSection/>
+        </>
+    )
+}
+
+export default Main
